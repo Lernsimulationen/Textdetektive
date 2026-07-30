bf78d19f0d90722d5a10a79e1858bcfb7423ceb0
Lernsimulationen <beuermann.jens@gmail.com>
2026-07-30 13:37:05 +0200

    up

---
+++ lernsimulationen/kurse/q1/KI Mensch und Verantwortung Gemini/sw.js
@@ -0,0 +1,37 @@
+const CACHE_NAME = 'ki-ethik-kommission-v2';
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll([
+        './',
+        './index.html',
+        './manifest.json'
+      ]);
+    }).then(() => self.skipWaiting())
+  );
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cache) => {
+          if (cache !== CACHE_NAME) {
+            return caches.delete(cache);
+          }
+        })
+      );
+    }).then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request).catch(() => {
+        return caches.match('./index.html');
+      });
+    })
+  );
+});
